7c246dd959e97e141857ad672662a6af1e99fcd5
ardiantohendy <bayuardianto421@gmail.com>
2024-05-24 04:01:01 +0700

    new commit

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,11 +4,24 @@ pragma solidity 0.8.18;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage [] public listOfSimpleStorageContract;
+    SimpleStorage [] public listOfSimpleStorageContracts;
     
     function createSimpleStorageContract() public {
         SimpleStorage newSimpleStorageContract = new SimpleStorage();
-        listOfSimpleStorageContract.push(newSimpleStorageContract);
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
     }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public  {
+        //need
+        // address
+        //ABI
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex]; 
+        return mySimpleStorage.retrieve();
+    } 
 
 }
